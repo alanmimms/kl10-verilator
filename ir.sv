a896a868ab874e36f3bdb24e8ec29c3ceada5400
Alan Mimms <alanmimms@gmail.com>
2020-05-31 10:45:46 -0700

    checkpoint

--- ir.sv
+++ ir.sv
@@ -20,13 +20,13 @@
 // I didn't waste my (well, ... hobby) time tediously cramming DRAM
 // into 256 words with a rubegoldbergian 256x3 even/odd scheme. Here
 // we implement all 512 words of A[0:2], B:[0,2], P, J[1:4], J[7:10]
-// or 512*15 = a big throbbing 7.5kilobits of RAM. I did avoid storing
-// J[5:6] since they have to be zero anyway.
+// without the padding used in the microcode listings as or 512*15 = a
+// big throbbing 7.5Kb of RAM.
 //
-// I have simplified
-// the design by "wasting" a small amount of RAM. Sue me. It's the
-// fucking 21st Century and we stand SO TALL on the shoulders of
-// giants that we don't have to dick around to save a few kilobits.
+// The design is simplified by "wasting" a small amount of RAM. Sue
+// me. It's the fucking 21st Century and we stand SO TALL on the
+// shoulders of giants that we don't have to dick around to save a few
+// kilobits.
 `timescale 1ns/1ps
 `include "ebox.svh"
 
@@ -48,32 +48,8 @@ module ir(iIR IR,
   bit [0:DRAM_WIDTH-1] DRAMdata;
   bit [0:DRAM_ADDR_BITS - 1] DRADR;
 
-  bit [8:10] DRAM_J_X, DRAM_J_Y;
-
   bit IR_CLK;
   assign IR_CLK = CLK.IR;
-
-  bit dramLoadXYeven, dramLoadXYodd, EN_IO_JRST, EN_AC;
-  bit dramLoadJcommon, dramLoadJeven, dramLoadJodd;
-
-`ifdef KL10PV_TB
-  sim_mem
-    #(.SIZE(DRAM_SIZE), .WIDTH(DRAM_WIDTH), .NBYTES(1))
-  dram
-    (.clk(IR_CLK),
-     .din('0),                    // XXX
-     .dout(DRAMdata),
-     .addr(DRADR),
-     .oe(1'b1),
-     .wea(1'b0));                   // XXX
-`else
-  dram_mem dram(.clka(IR_CLK),
-                .addra(DRADR),
-                .douta(DRAMdata),
-                .dina('0),
-                .wea(1'b0),
-                .ena(1'b1));
-`endif
 
   // p.210 shows older KL10 DRAM addressing.
 
@@ -100,20 +76,30 @@ module ir(iIR IR,
 
   always @(posedge CON.LOAD_DRAM) DRADR <= {IR.IR[0:2], instr7XX ? IR.IR[3:8] : ioDRADR};
 
-  bit [0:2] DRAM_A_X, DRAM_A_Y, DRAM_B_X, DRAM_B_Y;
   bit [7:10] DRAM_PAR_J;
-  bit DRAM_PAR, DRAM_PAR_X, DRAM_PAR_Y;
+  bit DRAM_PAR;
 
   bit HOLD_DRAM, HOLD_IR;
   assign HOLD_DRAM = ~CON.LOAD_DRAM | CLK.IR;
   assign HOLD_IR = ~CON.LOAD_IR | CLK.IR;
 
   // Latch-mux es
-  always_latch if (HOLD_DRAM) IR.DRAM_A = DRADR[8] ? DRAM_A_Y : DRAM_A_X;
-  always_latch if (HOLD_DRAM) DRAM_PAR = DRADR[8] ? DRAM_PAR_Y : DRAM_PAR_X;
-  always_latch if (HOLD_DRAM) IR.DRAM_B = DRADR[8] ? DRAM_B_Y : DRAM_B_X;
+
+  // In this design, the DRADR[8] address pin simply goes to our DRAM
+  // RAM to avoid having to do this messy muxing. I wrote this code
+  // before I realized this completely, so here it is in case I have
+  // to go back to the frugality of RAM bits that was only somewhat
+  // justified in the 1970s.
+  //
+  // E52
+  // always_latch if (HOLD_DRAM) IR.DRAM_A = DRADR[8] ? DRAM_A_Y : DRAM_A_X;
+  // always_latch if (HOLD_DRAM) DRAM_PAR = DRADR[8] ? DRAM_PAR_Y : DRAM_PAR_X;
+  // E33
+  // always_latch if (HOLD_DRAM) IR.DRAM_B = DRADR[8] ? DRAM_B_Y : DRAM_B_X;
+  // E63
+  // always_latch if (HOLD_DRAM) IR.DRAM_J[1:4] = DRADR[8] ? DRAM_J_Y[7:10] : DRAM_J_Y[7:10];
+
   always_latch if (HOLD_DRAM) IR.DRAM_J[1:4] = ~IR_JRST ? DRAM_J[1:4] : {DRAM_J[1:3], 1'b0};
-  always_latch if (HOLD_DRAM) IR.DRAM_J[1:4] = DRADR[8] ? DRAM_J_Y[7:10] : DRAM_J_Y[7:10];
   always_latch if (HOLD_DRAM) IR.DRAM_J[7:10] = ~IR_JRST ? IR.IR[9:12] : DRAM_PAR_J[7:10];
   always_latch if (HOLD_DRAM) IR.AC[9:12] = ~IR_EN_AC ? 4'b0000 : IR.IR[9:12];
 
@@ -123,18 +109,56 @@ module ir(iIR IR,
   always_comb DR_ADR[3:5] == INSTR_7xx ? {3{e75q2}} | IR.IR[7:9] : IR.IR[3:5];
   always_comb DR_ADR[6:8] == INSTR_7xx ? IR.IR[10:12] : IR.IR[6:8];
 
-  // Latch-mux
-  always_latch if (HOLD_IR) IR.IR = ~CLK.MB_XFER ? EDP.AD[0:12] : MBOX.CACHE_DATA[0:12];
-
+  // XXX In addition to the below, this has two mystery OR-term
+  // signals on each input to the AND that are unlabeled except for
+  // backplane references <ES2> and <ER2>. See E66 p.128.
+  assign IR.IO_LEGAL = &IR.IR[3:6];
   assign IR.ACeq0 = IR.IR[9:12] == 4'b0;
 
-  // XXX In addition to the below, this has two mystery OR term
-  // signals on each input to the AND that are unlabeled except for
-  // backplane references ES2 and ER2. See E66 p.128.
-  assign IR.IO_LEGAL = &IR.IR[3:6];
+  // The actual IR register is built out of 10173 latch-muxes.
+  always_latch if (HOLD_IR) IR.IR[0:12] = CLK.MB_XFER ? MBOX.CACHE_DATA[0:12] : EDP.AD[0:12];
 
 
   // IR2 p.129
+`ifdef KL10PV_TB
+  sim_mem
+    #(.SIZE(DRAM_SIZE), .WIDTH(DRAM_WIDTH), .NBYTES(1))
+  dram
+    (.clk(IR_CLK),
+     .din('0),                  // XXX
+     .dout(DRAMdata),
+     .addr(DRADR),
+     .oe(1'b1),
+     .wea(1'b0));               // XXX
+`else
+  dram_mem dram(.clka(IR_CLK),
+                .addra(DRADR),
+                .douta(DRAMdata),
+                .dina('0),
+                .wea(1'b0),
+                .ena(1'b1));
+`endif
+
+/*
+  // always_latch if (HOLD_DRAM) IR.DRAM_A = DRADR[8] ? DRAM_A_Y : DRAM_A_X;
+  // always_latch if (HOLD_DRAM) DRAM_PAR = DRADR[8] ? DRAM_PAR_Y : DRAM_PAR_X;
+  // E33
+  // always_latch if (HOLD_DRAM) IR.DRAM_B = DRADR[8] ? DRAM_B_Y : DRAM_B_X;
+  // E63
+  // always_latch if (HOLD_DRAM) IR.DRAM_J[1:4] = DRADR[8] ? DRAM_J_Y[7:10] : DRAM_J_Y[7:10];
+
+  always_latch if (HOLD_DRAM) IR.DRAM_J[1:4] = ~IR_JRST ? DRAM_J[1:4] : {DRAM_J[1:3], 1'b0};
+  always_latch if (HOLD_DRAM) IR.DRAM_J[7:10] = ~IR_JRST ? IR.IR[9:12] : DRAM_PAR_J[7:10];
+  always_latch if (HOLD_DRAM) IR.AC[9:12] = ~IR_EN_AC ? 4'b0000 : IR.IR[9:12];
+*/
+
+  // These bit field definitions come from the tools/write-dram-mem.js
+  // packing.
+  assign IR.DRAM_A = DRAMdata[0:2];
+  assign IR.DRAM_B = DRAMdata[3:5];
+  assign IR.DRAM_PAR = DRAMdata[6];
+  assign IR.DRAM_J[1:4] = DRAMdata[7:10];
+  assign IR.DRAM_J[7:10] = DRAMdata[11:14];
 
 
   assign magic7eq8 = CRAM.MAGIC[7] ^ CRAM.MAGIC[8];
