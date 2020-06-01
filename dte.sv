cfdfa6f84d04779e396aa2e04fb1741eff9313ce
Alan Mimms <alanmimms@gmail.com>
2020-05-31 21:56:55 -0700

    checkpoint

--- dte.sv
+++ dte.sv
@@ -5,13 +5,20 @@ module dte(input bit clk,
            iMBOX MBOX);
 
   import "DPI-C" function void DTEtick(input bit CROBAR,
-                                       input longint tickCount);
+                                       input longint ns);
 
   initial begin
+    $display($time, " starting");
+
+    repeat (100) begin
+      @(negedge clk) ;
+    end
+
+    $display($time, " 100 clocks later");
   end
 
   final begin
   end
 
-  always @(posedge clk) DTEtick(CROBAR, $time);
+  always @(posedge clk) DTEtick(CROBAR, $time / 1000);
 endmodule
