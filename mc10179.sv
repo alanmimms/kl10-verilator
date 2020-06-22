d5922a05dad087f257c8324849831e736f84316f
Alan Mimms <alanmimms@gmail.com>
2020-06-21 18:50:48 -0700

    Clean up - maybe fixes too?

--- mc10179.sv
+++ mc10179.sv
@@ -8,17 +8,20 @@ module mc10179(input bit [0:3] G,
                output bit C8OUT,
                output bit C2OUT);
 
-  assign C8OUT = ~|{~G[3],
-                    ~|{P[3], G[2]},
-                    ~|{P[3], P[2], G[1]},
-                    ~|{P[3], P[2], P[1], G[0]},
-                    ~|{P[3], P[2], P[1], P[0], CIN}};
-  assign C2OUT = ~|{~G[1],
-                    ~|{P[1], G[0]},
-                    ~|{P[1], P[0], CIN}};
-  assign GG = ~|{~G[3],
-                 ~|{P[3], G[2]},
-                 ~|{P[3], P[2], G[1]},
-                 ~|{P[3], P[2], P[1], G[0]}};
-  assign PG = P[3] | P[2] | P[1] | P[0];
+  bit [3:0] Gle, Ple;
+  assign Gle = {G[0], G[1], G[2], G[3]};
+  assign Ple = {P[0], P[1], P[2], P[3]};
+  assign C8OUT = ~(~Gle[3] |
+                   ~(Ple[3] | Gle[2]) |
+                   ~(Ple[3] | Ple[2] | Gle[1]) |
+                   ~(Ple[3] | Ple[2] | Ple[1] | Gle[0]) |
+                   ~(Ple[3] | Ple[2] | Ple[1] | Ple[0] | CIN));
+  assign C2OUT = ~(~Gle[1] |
+                   ~(Ple[1] | Gle[0]) |
+                   ~(Ple[1] | Ple[0] | CIN));
+  assign GG = ~(~Gle[3] |
+                ~(Ple[3] | Gle[2]) |
+                ~(Ple[3] | Ple[2] | Gle[1]) |
+                ~(Ple[3] | Ple[2] | Ple[1] | Gle[0]));
+  assign PG = |P;
 endmodule // mc10179
