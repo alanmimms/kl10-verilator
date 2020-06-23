d0c430de5873f95b313cf940cea65183ad089e53
Alan Mimms <alanmimms@gmail.com>
2020-06-23 14:24:01 -0700

    Add more test cases. Rename to adAx2 and adAx2plus1

--- tb/edptb.sv
+++ tb/edptb.sv
@@ -28,6 +28,7 @@ module edptb(input clk);
 
   edp edp0(.*);
 
+  bit [0:35] sb;
   bit [5:0] state;
   initial state = '0;
   
@@ -84,14 +85,15 @@ module edptb(input clk);
     // Load AR with 123456789
     6'o01: begin
       $display($time, " set AR=h555555555");
+      sb = 36'h555555555;
       MBOX.CACHE_DATA[0:35] <= 36'h555555555;
       CRAM.AR <= arCACHE;
       CTL.AR00to08_LOAD <= 1;
       CTL.AR09to17_LOAD <= 1;
       CTL.ARR_LOAD <= 1;
-      CRAM.BR <= 0;
+      CRAM.BR <= brRECIRC;
       CRAM.ARX <= arxARX;
-      CRAM.BRX <= 0;
+      CRAM.BRX <= brxRECIRC;
     end
     6'o02: begin
       CTL.AR00to08_LOAD <= 0;
@@ -99,10 +101,11 @@ module edptb(input clk);
       CTL.ARR_LOAD <= 0;
     end
     
-    6'o04: $display($time, " AR=h%09X SB h555555555", EDP.AR[0:35]);
+    6'o04: $display($time, " AR=h%09X SB=h%09X", EDP.AR[0:35], sb);
 
     // Try AD/A first
     6'o10: begin
+      sb = 36'h123456789;
       MBOX.CACHE_DATA[0:35] <= 36'h123456789;
       CRAM.AR <= arCACHE;
       CTL.AR00to08_LOAD <= 1;
@@ -110,13 +113,13 @@ module edptb(input clk);
       CTL.ARR_LOAD <= 1;
       CRAM.AD <= adA;
       CRAM.ADA <= adaAR;
-      CRAM.BR <= 0;
+      CRAM.BR <= brRECIRC;
       CRAM.ARX <= arxARX;
-      CRAM.BRX <= 0;
+      CRAM.BRX <= brxRECIRC;
     end
     
     6'o11: begin
-      $display($time, " result: AD/A, ADA/AR, AR/AR AD=h%09x SB h555555555", EDP.AD[-2:35]);
+      $display($time, " result: AD/A, ADA/AR, AR/AR AD=h%09x SB=h%09X", EDP.AD[-2:35], sb);
       CTL.AR00to08_LOAD <= 0;
       CTL.AR09to17_LOAD <= 0;
       CTL.ARR_LOAD <= 0;
@@ -125,38 +128,115 @@ module edptb(input clk);
     // Load BR with 987654321
     6'o20: begin
       $display($time, " set BR=h987654321");
+      sb = 36'h987654321;
       MBOX.CACHE_DATA[0:35] <= 36'h987654321;
       CRAM.AR <= arCACHE;
       CTL.AR00to08_LOAD <= 1;
       CTL.AR09to17_LOAD <= 1;
       CTL.ARR_LOAD <= 1;
-      CRAM.BR <= brAR;
-      CRAM.ARX <= arxARX;
-      CRAM.BRX <= 0;
     end
     6'o21: begin
-      $display($time, " BR=h%09X SB h987654321", EDP.BR[0:35]);
       CTL.AR00to08_LOAD <= 0;
       CTL.AR09to17_LOAD <= 0;
       CTL.ARR_LOAD <= 0;
+      CRAM.BR <= brAR;
+      CRAM.ARX <= arxARX;
+      CRAM.BRX <= brxRECIRC;
+    end
+    6'o22: begin
+      $display($time, " BR=h%09X SB=h%09X", EDP.BR[0:35], sb);
+      CRAM.BR <= brRECIRC;
     end
     
     // Try AD/B
     6'o30: begin
-      $display($time, " try AD/B AR=%0x BR=%0x", EDP.AR, EDP.BR);
+      $display($time, " try AD/B AR=%09X BR=%09X SB=h%09X", EDP.AR, EDP.BR, sb);
+      sb = 36'h987654321;
       CRAM.AD <= adB;
+      CRAM.ADA <= adaAR;
+      CRAM.ADB <= adbBR;
+      CRAM.AR <= arAD;
+      CTL.AR00to08_LOAD <= 1;
+      CTL.AR09to17_LOAD <= 1;
+      CTL.ARR_LOAD <= 1;
+      CRAM.BR <= brRECIRC;
+      CRAM.ARX <= arxARX;
+      CRAM.BRX <= brxRECIRC;
+    end
+    
+    6'o31: $display($time, " result: AD/B, ADA/AR, ADB/BR AD=h%09x SB=h%09X", EDP.AD[-2:35], sb);
+
+    // Try AD/A+B
+    6'o40: begin
+      $display($time, " try AD/A+B AR=%09X BR=%09X SB=h%09X", EDP.AR, EDP.BR, sb);
+      sb = 36'hAAAAAAAAA;
+      CRAM.AD <= adAplusB;
+      CRAM.ADA <= adaAR;
+      CRAM.ADB <= adbBR;
+      MBOX.CACHE_DATA[0:35] <= 36'h123456789;
+      CRAM.AR <= arCACHE;
+      CTL.AR00to08_LOAD <= 1;
+      CTL.AR09to17_LOAD <= 1;
+      CTL.ARR_LOAD <= 1;
+      CRAM.BR <= brRECIRC;
+      CRAM.ARX <= arxARX;
+      CRAM.BRX <= brxRECIRC;
+    end
+    
+    6'o41: $display($time, " result: AD/A+B, ADA/AR, ADB/BR AD=h%09x SB=h%09X", EDP.AD[-2:35], sb);
+
+    // Try AD/ORCB+1
+    6'o47: begin
+      MBOX.CACHE_DATA[0:35] <= 36'o007757777;
+      CRAM.AR <= arCACHE;
+      CTL.AR00to08_LOAD <= 1;
+      CTL.AR09to17_LOAD <= 1;
+      CTL.ARR_LOAD <= 1;
+    end
+
+    6'o50: begin
+      $display($time, " try AD/ORCB+1 AR=%09X BR=%09X SB=h%09X", EDP.AR, EDP.BR, sb);
+      sb = 36'hFFFFFFFF;
+      CRAM.AD <= adORCBplus1;
+      CRAM.ADA <= adaAR;
+      CRAM.ADB <= adbBR;
+      MBOX.CACHE_DATA[0:35] <= 36'o007757777;
+      CRAM.AR <= arCACHE;
+      CTL.AR00to08_LOAD <= 0;
+      CTL.AR09to17_LOAD <= 0;
+      CTL.ARR_LOAD <= 0;
+      CRAM.BR <= brAR;
+      CRAM.ARX <= arxARX;
+      CRAM.BRX <= brxRECIRC;
+    end
+    
+    6'o51: $display($time, " result: AD/ORCB+1, ADA/AR, ADB/BR AD=h%09x SB=h%09X", EDP.AD[-2:35], sb);
+
+    // Try AD/Ax2
+    6'o57: begin
+      MBOX.CACHE_DATA[0:35] <= 36'h123456789;
+      CRAM.AR <= arCACHE;
+      CTL.AR00to08_LOAD <= 1;
+      CTL.AR09to17_LOAD <= 1;
+      CTL.ARR_LOAD <= 1;
+    end
+
+    6'o60: begin
+      $display($time, " try AD/Ax2 AR=%09X BR=%09X SB=h%09X", EDP.AR, EDP.BR, sb);
+      sb = 36'h2468ACF12;
+      CRAM.AD <= adAx2;
       CRAM.ADA <= adaAR;
       CRAM.ADB <= adbBR;
       CRAM.AR <= arAR;
       CTL.AR00to08_LOAD <= 0;
       CTL.AR09to17_LOAD <= 0;
       CTL.ARR_LOAD <= 0;
-      CRAM.BR <= 0;
+      CRAM.BR <= brAR;
       CRAM.ARX <= arxARX;
-      CRAM.BRX <= 0;
+      CRAM.BRX <= brxRECIRC;
     end
     
-    6'o31: $display($time, " result: AD/B, ADA/AR, ADB/BR AD=h%09x SB h987654321", EDP.AD[-2:35]);
+    6'o61: $display($time, " result: AD/ORCB+1, ADA/AR, ADB/BR AD=h%09x SB=h%09X", EDP.AD[-2:35], sb);
 
     6'o77: $finish;
     default: ;
