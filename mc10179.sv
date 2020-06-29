3ba9b942fbc3175c836a3e95c614712d32cc2416
Alan Mimms <alanmimms@gmail.com>
2020-06-29 09:07:25 -0700

    Recode as product of sums

--- mc10179.sv
+++ mc10179.sv
@@ -11,17 +11,17 @@ module mc10179(input bit [0:3] G,
   bit [3:0] Gle, Ple;
   assign Gle = {G[0], G[1], G[2], G[3]};
   assign Ple = {P[0], P[1], P[2], P[3]};
-  assign C8OUT = ~(~Gle[3] |
-                   ~(Ple[3] | Gle[2]) |
-                   ~(Ple[3] | Ple[2] | Gle[1]) |
-                   ~(Ple[3] | Ple[2] | Ple[1] | Gle[0]) |
-                   ~(Ple[3] | Ple[2] | Ple[1] | Ple[0] | CIN));
-  assign C2OUT = ~(~Gle[1] |
-                   ~(Ple[1] | Gle[0]) |
-                   ~(Ple[1] | Ple[0] | CIN));
-  assign GG = ~(~Gle[3] |
-                ~(Ple[3] | Gle[2]) |
-                ~(Ple[3] | Ple[2] | Gle[1]) |
-                ~(Ple[3] | Ple[2] | Ple[1] | Gle[0]));
+  assign C8OUT = Gle[3] &
+                 (Ple[3] | Gle[2]) &
+                 (Ple[3] | Ple[2] | Gle[1]) &
+                 (Ple[3] | Ple[2] | Ple[1] | Gle[0]) &
+                 (Ple[3] | Ple[2] | Ple[1] | Ple[0] | CIN);
+  assign C2OUT = Gle[1] &
+                 (Ple[1] | Gle[0]) &
+                 (Ple[1] | Ple[0] | CIN);
+  assign GG = Gle[3] &
+              (Ple[3] | Gle[2]) &
+              (Ple[3] | Ple[2] | Gle[1]) &
+              (Ple[3] | Ple[2] | Ple[1] | Gle[0]);
   assign PG = |P;
 endmodule // mc10179
