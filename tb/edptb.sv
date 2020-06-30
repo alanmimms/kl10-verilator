ecf87316fc24846bccd3430dd272b624325e2685
Alan Mimms <alanmimms@gmail.com>
2020-06-29 18:10:45 -0700

    Add tests for A*2 MC10181 function for all possible A/B/CIN

--- tb/edptb.sv
+++ tb/edptb.sv
@@ -26,12 +26,25 @@ module edptb(input clk);
                            1'b0,      // [22] Has master oscillator (not needed here)
                            13'd4001}; // [23:35] Serial number
 
-  edp edp0(.*);
-
   bit [0:35] sb;
-  bit [8:0] state;
+  bit [17:0] state;
   initial state = '0;
   
+  edp edp0(.*);
+
+  bit [0:3] s1, a1, b1, f1;
+  bit m1, cin1, cg1, cp1, cout1;
+  bit [-1:3] result1, sb1;
+
+  assign a1 = 4'(state[3:0]);
+  assign b1 = 4'(state[7:4]);
+  assign cin1 = 1'(state[8]);
+  assign m1 = 0;
+  assign s1 = 4'h3;
+  assign result1 = {cout1, f1};
+
+  mc10181 alu1(.S(s1), .M(m1), .A(a1), .B(b1), .CIN(cin1), .F(f1), .CG(cg1), .CP(cp1), .COUT(cout1));
+
   always @(posedge clk) state <= state + 1;
 
   assign CLK.EDP = clk;
@@ -41,8 +54,8 @@ module edptb(input clk);
   assign CTL.AD_CRY_36 = CRAM.AD[0];
 
   always @(posedge clk) begin
-    unique case (state)
-    9'o00: begin                // Initialization
+    case (state)
+    18'o00: begin                // Initialization
       $display($time, " [Start EDP test bench]");
 
       CTL.ADX_CRY_36 <= 0;
@@ -83,7 +96,7 @@ module edptb(input clk);
     end
 
     // Load AR with 123456789
-    9'o01: begin
+    18'o01: begin
       $display($time, " set AR=h555555555");
       sb = 36'h555555555;
       MBOX.CACHE_DATA[0:35] <= 36'h555555555;
@@ -95,16 +108,16 @@ module edptb(input clk);
       CRAM.ARX <= arxARX;
       CRAM.BRX <= brxRECIRC;
     end
-    9'o02: begin
+    18'o02: begin
       CTL.AR00to08_LOAD <= 0;
       CTL.AR09to17_LOAD <= 0;
       CTL.ARR_LOAD <= 0;
     end
     
-    9'o04: $display($time, " AR=h%09X SB=h%09X", EDP.AR[0:35], sb);
+    18'o04: $display($time, " AR=h%09X SB=h%09X", EDP.AR[0:35], sb);
 
     // Try AD/A first
-    9'o10: begin
+    18'o10: begin
       sb = 36'h123456789;
       MBOX.CACHE_DATA[0:35] <= 36'h123456789;
       CRAM.AR <= arCACHE;
@@ -118,7 +131,7 @@ module edptb(input clk);
       CRAM.BRX <= brxRECIRC;
     end
     
-    9'o11: begin
+    18'o11: begin
       $display($time, " result: AD/A, ADA/AR, AR/AR AD=h%09x SB=h%09X", EDP.AD[-2:35], sb);
       CTL.AR00to08_LOAD <= 0;
       CTL.AR09to17_LOAD <= 0;
@@ -126,7 +139,7 @@ module edptb(input clk);
     end
 
     // Load BR with 987654321
-    9'o20: begin
+    18'o20: begin
       $display($time, " set BR=h987654321");
       sb = 36'h987654321;
       MBOX.CACHE_DATA[0:35] <= 36'h987654321;
@@ -135,7 +148,7 @@ module edptb(input clk);
       CTL.AR09to17_LOAD <= 1;
       CTL.ARR_LOAD <= 1;
     end
-    9'o21: begin
+    18'o21: begin
       CTL.AR00to08_LOAD <= 0;
       CTL.AR09to17_LOAD <= 0;
       CTL.ARR_LOAD <= 0;
@@ -143,13 +156,13 @@ module edptb(input clk);
       CRAM.ARX <= arxARX;
       CRAM.BRX <= brxRECIRC;
     end
-    9'o22: begin
+    18'o22: begin
       $display($time, " BR=h%09X SB=h%09X", EDP.BR[0:35], sb);
       CRAM.BR <= brRECIRC;
     end
     
     // Try AD/B
-    9'o30: begin
+    18'o30: begin
       $display($time, " try AD/B AR=%09X BR=%09X SB=h%09X", EDP.AR, EDP.BR, sb);
       sb = 36'h987654321;
       CRAM.AD <= adB;
@@ -164,10 +177,10 @@ module edptb(input clk);
       CRAM.BRX <= brxRECIRC;
     end
     
-    9'o31: $display($time, " result: AD/B, ADA/AR, ADB/BR AD=h%09x SB=h%09X", EDP.AD[-2:35], sb);
+    18'o31: $display($time, " result: AD/B, ADA/AR, ADB/BR AD=h%09x SB=h%09X", EDP.AD[-2:35], sb);
 
     // Try AD/A+B
-    9'o40: begin
+    18'o40: begin
       $display($time, " try AD/A+B AR=%09X BR=%09X SB=h%09X", EDP.AR, EDP.BR, sb);
       sb = 36'hAAAAAAAAA;
       CRAM.AD <= adAplusB;
@@ -183,11 +196,11 @@ module edptb(input clk);
       CRAM.BRX <= brxRECIRC;
     end
     
-    9'o41: $display($time, " result: AD/A+B, ADA/AR, ADB/BR AD=h%09x SB=h%09X", EDP.AD[-2:35], sb);
+    18'o41: $display($time, " result: AD/A+B, ADA/AR, ADB/BR AD=h%09x SB=h%09X", EDP.AD[-2:35], sb);
 
     // Try AD/ORCB+1
     // TODO: LH of AD is wrong
-    9'o47: begin
+    18'o47: begin
       MBOX.CACHE_DATA[0:35] <= 36'o007757777;
       CRAM.AR <= arCACHE;
       CTL.AR00to08_LOAD <= 1;
@@ -195,7 +208,7 @@ module edptb(input clk);
       CTL.ARR_LOAD <= 1;
     end
 
-    9'o50: begin
+    18'o50: begin
       $display($time, " try AD/ORCB+1 AR=%09X BR=%09X SB=h%09X", EDP.AR, EDP.BR, sb);
       sb = 36'hFFFFFFFF;
       CRAM.AD <= adORCBplus1;
@@ -211,10 +224,10 @@ module edptb(input clk);
       CRAM.BRX <= brxRECIRC;
     end
     
-    9'o51: $display($time, " result: AD/ORCB+1, ADA/AR, ADB/BR AD=h%09x SB=h%09X", EDP.AD[-2:35], sb);
+    18'o51: $display($time, " result: AD/ORCB+1, ADA/AR, ADB/BR AD=h%09x SB=h%09X", EDP.AD[-2:35], sb);
 
     // Try AD/Ax2
-    9'o57: begin
+    18'o57: begin
       MBOX.CACHE_DATA[0:35] <= 36'h123456789;
       CRAM.AR <= arCACHE;
       CTL.AR00to08_LOAD <= 1;
@@ -222,7 +235,7 @@ module edptb(input clk);
       CTL.ARR_LOAD <= 1;
     end
 
-    9'o60: begin
+    18'o60: begin
       $display($time, " try AD/Ax2 AR=%09X BR=%09X SB=h%09X", EDP.AR, EDP.BR, sb);
       sb = 36'h2468ACF12;
       CRAM.AD <= adAx2;
@@ -237,10 +250,10 @@ module edptb(input clk);
       CRAM.BRX <= brxRECIRC;
     end
     
-    9'o61: $display($time, " result: AD/Ax2, ADA/AR, ADB/BR AD=h%09x SB=h%09X", EDP.AD[-2:35], sb);
+    18'o61: $display($time, " result: AD/Ax2, ADA/AR, ADB/BR AD=h%09x SB=h%09X", EDP.AD[-2:35], sb);
 
     // Try overflow
-    9'o67: begin
+    18'o67: begin
       MBOX.CACHE_DATA[0:35] <= 36'hFFFFFFFFF;
       CRAM.AR <= arCACHE;
       CTL.AR00to08_LOAD <= 1;
@@ -248,7 +261,7 @@ module edptb(input clk);
       CTL.ARR_LOAD <= 1;
     end
 
-    9'o70: begin
+    18'o70: begin
       $display($time, " try overflow AD/A+1 AR=%09X BR=%09X SB=h%09X", EDP.AR, EDP.BR, sb);
       sb = 36'h000000000;
       CRAM.AD <= adAplus1;
@@ -263,10 +276,16 @@ module edptb(input clk);
       CRAM.BRX <= brxRECIRC;
     end
     
-    9'o71: $display($time, " result: AD/A+1, ADA/AR, ADB/BR AD=h%09x SB=h%09X", EDP.AD[-2:35], sb);
+    18'o71: $display($time, " result: AD/A+1, ADA/AR, ADB/BR AD=h%09x SB=h%09X", EDP.AD[-2:35], sb);
 
-    9'o77: $finish;
-    default: ;
+    // Try every value of A and B for S=3 (A*2) with CIN and COUT.
+    default: begin
+      sb1 = {1'b0, a1} * 5'h2 + 5'(cin1);
+      if (result1 != sb1) $display(" MISMATCH: %0b*2 was %0b sb %0b", a1, result1, sb1);
+    end
+
+    18'o002001: $finish;
+
     endcase
   end
 endmodule
