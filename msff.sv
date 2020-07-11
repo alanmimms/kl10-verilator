cf87206718662c9f0448063c9a74850d6b1b0c47
Alan Mimms <alanmimms@gmail.com>
2020-07-10 18:17:36 -0700

    Is this how it should work?

--- msff.sv
+++ msff.sv
@@ -1,12 +1,13 @@
 `timescale 1ns/1ps
 // Master/slave (HATE that name!) flipflop emulating one flop of
-// MC10176. Latch data on negedge of clk and present on following
-// posedge.
+// MC10176. Master latches data while clk is low and presents to
+// output on following posedge.
 module msff(input bit clk,
             output bit q,
             input bit d);
 
   bit master;
-  always_ff @(negedge clk) master <= d;
+
+  always_latch @(d) if (~clk) master <= d;
   always_ff @(posedge clk) q <= master;
 endmodule
